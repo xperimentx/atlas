1d8a02ff65c8f70990e4a70494f16e399e94f6b9
Roberto Gonzalez Vazquez <roberto.experiments@gmail.com>
2017-11-16 00:42:35 +0100

    Autoloader - aplha

---
+++ atlas/_doc_.php
@@ -0,0 +1,8 @@
+<?php
+namespace Atlas; 
+
+/** @const IGNORE_AUTOLOADER If is defined ignore atlas autoloder */
+const IGNORE_AUTOLOADER = NULL;
+
+/** @const Atlas\CFG_FILE  If defined is an optional configuration file for load after /cfg/atlas.php */
+const CFG_FILE = NULL;
---
+++ atlas/auto.php
@@ -0,0 +1,30 @@
+<?php
+namespace Atlas;
+/**
+ * ATLAS auto initilize a loader
+ * @author Roberto Gonzalez Vazquez
+ */
+ 
+
+/** @const Application root path */
+define('Atlas\ROOT_PATH', substr (__DIR__, 0, -6 )); //-"/atlas"
+
+
+// Autoloader 
+if (!defined('Atlas\IGNORE_AUTOLOADER'))  
+    include ROOT_PATH."/autoloader.php";
+
+// Load main confifuration file
+if (file_exists(ROOT_PATH. '/cfg/atlas.php'))
+    include_once ROOT_PATH. '/cfg/atlas.php';
+
+
+// Load optional configuration file
+if (defined('Atlas\CFG_FILE'))          
+     include_once ROOT_PATH. CFG_FILE ;
+
+               
+ 
+
+
+ 
